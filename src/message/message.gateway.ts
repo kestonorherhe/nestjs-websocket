d1334f39fd21a386ba5499409d61003990b4dc8e
kestonorherhe <36920276+kestonorherhe@users.noreply.github.com>
2023-05-18 13:58:43 +0100

    first commit

---
+++ src/message/message.gateway.ts
@@ -0,0 +1,46 @@
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { MessagesService } from './messages.service';
+
+@WebSocketGateway({ cors: true })
+export class MessageGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messagesService: MessagesService) {}
+
+  private logger: Logger = new Logger('MessageGateway');
+
+  @WebSocketServer() wss: Server;
+
+  afterInit(server: Server) {
+    this.logger.log('Initialized');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client Disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client Connected: ${client.id}`);
+  }
+
+  // @SubscribeMessage('getMessages')
+  // async handleGetMessage(client: Socket): Promise<void> {
+  //   const messages = await this.messagesService.getAllMessages();
+  //   this.wss.emit('receiveMessage', messages);
+  // }
+
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(client: Socket, payload: string): Promise<void> {
+    const newMessage = await this.messagesService.createMessage(payload);
+    this.wss.emit('receiveMessage', newMessage);
+  }
+}
